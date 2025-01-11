9adc7fc8ae3dd3d69da2add4f833c935ff924be3
GilAce <gilacevedo96@gmail.com>
2025-01-11 06:33:11 +0000

    Enabled saving generated workouts to MongoDB

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,17 @@ import re
 from openai import OpenAI
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from datetime import datetime
+
 
 OPENAI_API_KEY = st.secrets["openai_api_key"]
 SENDGRID_API_KEY = st.secrets["sendgrid_api_key"]
-
+ASSISTANT_ID = "asst_0BwF5ADrrHYMXacxytCSgrkS"
+MONGO_URI = st.secrets["mongo_uri"]
+DATABASE_NAME = "workout_coach"
+COLLECTION_NAME = "generated_workouts"
 
 def insert_google_links(workout_plan):
     # Find markers <<<Exercise>>> and replace them with markdown links
@@ -86,19 +93,61 @@ To ensure you’re off to a strong start, here are a few questions to help us re
         print(f"Error sending email: {e}")
         return False
 
-# Function to get workout plan from ChatGPT
+def save_workout_plan_to_mongo(assistant_id, thread_id, prompt, workout_plan):
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client[DATABASE_NAME]
+        collection = db[COLLECTION_NAME]
+        
+        document = {
+            "assistant_id": assistant_id,
+            "thread_id": thread_id,
+            "prompt": prompt,
+            "workout_plan": workout_plan,
+            "date_generated": datetime.utcnow()
+        }
+        
+        result = collection.insert_one(document)
+        print("Workout plan saved to MongoDB!")
+        document_id = result.inserted_id
 
+        return document_id
+        
+    except Exception as e:
+        print(f"Error saving to MongoDB: {e}")
+    finally:
+        client.close()
 
-def generate_workout_plan(goal, concerns, equipment, training_years, workout_duration, workout_type, focus_area):
-    prompt = f"""
-        - Goal: {goal}
-        - Injuries/Pains: {concerns}
-        - Equipment: {equipment}
-        - Training Years: {training_years}
-        - Duration: {workout_duration} minutes
-        - Workout Type: {workout_type}
-        - Focus Area: {focus_area}
-    """
+def update_workout_plan_with_email(document_id, email):
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client[DATABASE_NAME]
+        collection = db[COLLECTION_NAME]
+        
+        result = collection.update_one(
+            {"_id": ObjectId(document_id)},  # Use the saved _id
+            {"$set": {"email": email}}  # Add or update the email field
+        )
+        
+        if result.matched_count > 0:
+            print("Document updated successfully!")
+        else:
+            print("Document not found!")
+        
+    except Exception as e:
+        print(f"Error updating document: {e}")
+    finally:
+        client.close()
+
+def generate_workout_plan(goal, training_years, workout_duration, 
+                          concerns="None", equipment="None", workout_type="None", focus_area="None"):
+    prompt = f"""- Goal: {goal}
+- Injuries/Pains: {concerns}
+- Equipment: {equipment}
+- Training Years: {training_years}
+- Duration: {workout_duration} minutes
+- Workout Type: {workout_type}
+- Focus Area: {focus_area}"""
 
     client = OpenAI(
         api_key=OPENAI_API_KEY
@@ -114,22 +163,36 @@ def generate_workout_plan(goal, concerns, equipment, training_years, workout_dur
 
     run = client.beta.threads.runs.create_and_poll(
         thread_id=thread.id,
-        assistant_id="asst_0BwF5ADrrHYMXacxytCSgrkS",
+        assistant_id=ASSISTANT_ID,
     )
 
     if run.status == 'completed':
         messages = client.beta.threads.messages.list(
             thread_id=thread.id
         )
-        print(messages.data[0].content[0].text.value)
+        workout_plan = messages.data[0].content[0].text.value
+
     else:
         workout_plan = "Unable to generate workout plan. Please try again later."
 
-    workout_plan = messages.data[0].content[0].text.value
+    saved_document_id = save_workout_plan_to_mongo(
+        assistant_id=ASSISTANT_ID,
+        thread_id=thread.id,
+        prompt=prompt,
+        workout_plan=workout_plan
+    )
+    
+    print("Assistant ID:")
+    print(ASSISTANT_ID)
+    print("Thread ID:")
+    print(thread.id)
+    print("Prompt:")
+    print(prompt)
+    print("Workout Plan:")
+    print(workout_plan)
 
-    return workout_plan
 
-# Streamlit app
+    return workout_plan, saved_document_id
 
 
 def main():
@@ -168,27 +231,29 @@ def main():
 
     if st.button("Generate Program"):
         with st.spinner("Generating your custom program..."):
-            workout_plan = generate_workout_plan(
-                goal, concerns, equipment, training_years, workout_duration, workout_type, focus_area)
+            workout_plan, saved_document_id = generate_workout_plan(
+                goal, training_years, workout_duration, concerns, equipment, workout_type, focus_area)
             workout_plan = insert_google_links(workout_plan)
-            print(workout_plan)
             st.session_state['workout_plan'] = workout_plan
+            st.session_state['saved_document_id'] = saved_document_id
             st.success("Program generated!")
 
     # Step 2: Display workout plan and create dynamic pages
-    if "workout_plan" in st.session_state:
+    if "workout_plan" in st.session_state and "saved_document_id" in st.session_state:
         st.header("Here is your custom workout! Let's Go!")
         try:
             workout_plan = st.session_state['workout_plan']
+            saved_document_id = st.session_state['saved_document_id']
+
             st.markdown(workout_plan, unsafe_allow_html=True)
 
-            st.header(
-                "Don't lose your custom workout. Get it sent to your email!")
+            st.header("Don't lose your custom workout. Get it sent to your email!")
 
-            email = st.text_input("Enter your email address:")
+            email = st.text_input(label="Enter your email address:", placeholder="Example: awesome_person@cool_domain.com")
             if st.button("Send to Email"):
                 if email:
                     success = send_email(email, workout_plan)
+                    update_workout_plan_with_email(saved_document_id, email)
                     if success:
                         st.success("Workout plan sent successfully!")
                     else:
